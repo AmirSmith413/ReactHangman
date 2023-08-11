93431def6fc65542b98e127d22700b8eed42cd05
AmirSmith413 <amirsmith166@gmail.com>
2023-08-10 21:00:57 -0700

    first commit

---
+++ Hangman/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import HangedMan from './components/HangedMan'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+       <HangedMan/>
+
+
+   </>
+  )
+}
+
+export default App
---
+++ Hangman/src/components/HangedMan.tsx
@@ -0,0 +1,13 @@
+import React, {  useState } from 'react'
+
+const HangedMan = () => {
+    const [correctGuesses, setCorrectGuesses] = useState([])
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default HangedMan
